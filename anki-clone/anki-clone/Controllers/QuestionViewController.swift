9d1ffb07d6b56bc748ac76ceb60e2b290cc0ed22
Le0nX <cloudmindone@gmail.com>
2019-10-06 20:13:02 +0300

    init: init project settings

---
+++ anki-clone/anki-clone/Controllers/QuestionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  anki-clone
+//
+//  Created by Denis Nefedov on 06.10.2019.
+//  Copyright © 2019 X. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
