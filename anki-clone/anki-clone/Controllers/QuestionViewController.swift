d7134b40d7ddbc9783268e4c843c944df27151c0
Le0nX <cloudmindone@gmail.com>
2019-10-08 06:23:57 +0300

    feature: basic UI handling

--- anki-clone/anki-clone/Controllers/QuestionViewController.swift
+++ anki-clone/anki-clone/Controllers/QuestionViewController.swift
@@ -10,11 +10,57 @@ import UIKit
 
 class QuestionViewController: UIViewController {
 
+    //MARK: - Controller instances
+    public var questionGroup = QuestionGroup.basicPhrases()
+    public var questionIndex = 0
+    
+    public var correctAnswers = 0
+    public var incorrectAnswers = 0
+    
+    public var questionView: QuestionView? {
+        guard isViewLoaded else {return nil} // проверяем, что вью прогрузилась
+        return (view as! QuestionView)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showQuestion()
+    }
+    
+    private func showQuestion() {
+        let question = questionGroup.questions[questionIndex]
+        
+        questionView?.answerLabel.text = question.answer
+        questionView?.promptLabel.text = question.prompt
+        questionView?.hintLabel.text = question.hint
+        
+        questionView?.answerLabel.isHidden = true
+        questionView?.hintLabel.isHidden = true
     }
 
-
+    // MARK: - IBActions
+    @IBAction func toggleAnswerLabels(_ sender: Any) {
+        questionView?.answerLabel.isHidden = !(questionView?.answerLabel.isHidden)!
+        questionView?.hintLabel.isHidden = !(questionView?.hintLabel.isHidden)!
+    }
+    
+    @IBAction func handleCorrect(_ sender: Any) { correctAnswers += 1
+        questionView!.correctCountLabel.text = "\(correctAnswers)"
+        showNextQuestion()
+    }
+    // 2
+    @IBAction func handleIncorrect(_ sender: Any) { incorrectAnswers += 1
+        questionView!.incorrectCountLabel.text = "\(incorrectAnswers)"
+        showNextQuestion()
+    }
+    // 3
+    private func showNextQuestion() {
+        questionIndex += 1
+        guard questionIndex < questionGroup.questions.count else {
+            // TODO: - Handle this...!
+            return
+        }
+        showQuestion()
+    }
 }
 
